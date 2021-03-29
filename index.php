7f9d1a48a1f1f10b0232e93f0e4a02e3b13f3c5e
klimac <klimac@users.noreply.github.com>
2021-03-29 17:10:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
+from flask import Flask, request
+from flask_restful import Resource, Api
 
-phpinfo();
+app = Flask(__name__)
+api = Api(app)
 
-?>
+class Greeting (Resource):
+    def get(self):
+        return 'Hello World!'
+
+api.add_resource(Greeting, '/')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0','8080')
